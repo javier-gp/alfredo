c638e91e7db5212600a6b2794e0c969a01c77e69
Javi <javier@dreamstarcash.com>
2013-02-28 13:04:26 +0100

    Modified tests for pet project for testing Jenkins

---
+++ test/dummyTest.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('Dummy', function (){
+    it('should pass for checking Jenkins graphs', function(done){
+        assert.equal(1,1);
+        done();
+    });
+});
+
--- test/routes.js
+++
@@ -1,9 +0,0 @@
-var request = require('supertest'),
-    index = require('../routes'),
-    app = require('../app.js');
-
-describe('Routes', function (){
-    it('should have correct route and status when going to /index', function(done){
-        request(app).get('/').expect(200, done);
-    });
-});
